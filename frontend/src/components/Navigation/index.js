6e0ab7a7ea0a8727746d0f31f2ce9c3d1adb70eb
kisha onia <kisharoseonia@gmail.com>
2023-06-11 19:24:47 -0700

    NAV

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -9,6 +9,7 @@ import "./Navigation.css";
 import { login } from "../../store/session";
 import logoproj from "../../assets/logoproj.jpg";
 
+
 function Navigation({ isLoaded }) {
   const sessionUser = useSelector((state) => state?.session?.user);
 
@@ -36,22 +37,70 @@ function Navigation({ isLoaded }) {
   }
 
   return (
-    <div className="navbar">
-      <div className="left-nav-bar">
-        <a href="/">
-          <img src={logoproj} height="100px" width="300px" alt="Logo" />
-        </a>
-      </div>
-      <div className="right-nav-bar">
-        {sessionUser && isLoaded && (
-          <NavLink className="create-spot-link" exact to="/spots/new">
-            create a new spot
-          </NavLink>
-        )}
+    <div className="wrapper-navigation">
+      <div className="navbar">
+        <div className="left-nav-bar">
+          <a href="/">
+            <img src={logoproj} className="logo-home" height="100px" width="300px" alt="Logo" />
+          </a>
+        </div>
+        <div className="middle-navbar">
 
-        <button className="profile-button">
-          <ProfileButton user={sessionUser} />
-        </button>
+
+          <div className="homepage-input">
+            <input
+              className="homepage-date-check-in"
+              placeholder="Anywhere"
+              onfocus="(this.type = 'homepage-date-check-in')"
+              type="date"
+            ></input>
+            <input
+              className="homepage-date-check-out"
+              placeholder="Any week"
+              type="date"
+              onfocus="(this.type = 'homepage-date-check-in')">
+            </input>
+            <input
+              className="guest-check-in"
+              placeholder="Add Guests"
+              type="number">
+            </input>
+          </div>
+          <ul >
+            <div className="homepage-input-popup">
+              <li>Stay</li>
+              {/* Add popup search where destinations  STAY */}
+              <li>Experiences</li>
+              <li>Online Experience</li>
+            </div>
+          </ul>
+
+        </div>
+
+        <div className="right-nav-bar">
+
+          {sessionUser && isLoaded && (
+            <NavLink className="create-spot-link" exact to="/spots/new">
+              snowlfb your home
+            </NavLink>
+          )}
+
+          <button className="profile-button">
+            <ProfileButton user={sessionUser} />
+          </button>
+        </div>
+
+
+      </div>
+      <div className="footer">
+        <div className="kishas-footer">
+          <p className="kishas-name">Kisha Onia</p>
+          <p className="links-right">
+            <a className="github" href="https://github.com/kishaonia">Github</a>
+            <a className="portfolio" href="https://kishaonia.github.io/KishaOnia/">Portfolio</a>
+            <a className="email" href="mailto:workwithkisha@gmail.com">Email</a>
+          </p>
+        </div>
       </div>
     </div>
   );
